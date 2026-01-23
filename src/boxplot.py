480c98403c681b79a2928b901bb9f92049b39a31
ahmedyosifahmed <ahmed.y.ahmed@uconn.edu>
2026-01-22 21:27:12 -0500

    Display Boxplot in figs folder
    Updated boxplot.py code in the src folder, such that a boxplot can be displayed in the figs folder after running the code!

--- src/boxplot.py
+++ src/boxplot.py
@@ -1,5 +1,11 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
 
 # Load California Housing dataset
 housing = fetch_california_housing(as_frame=True)
@@ -12,3 +18,13 @@ print(df.head())
 print(df.shape)
 
 # you can save the boxplot...
+plt.figure(figsize=(8, 6))
+plt.boxplot(df["MedHouseVal"])
+plt.title("Boxplot of MedHouseValue")
+plt.ylabel("Value")
+plt.xlabel("MedHouseValue")
+
+# Save the plot
+plt.savefig("figs/MedHouseVal.png", dpi=300, bbox_inches="tight")
+plt.close()
+
