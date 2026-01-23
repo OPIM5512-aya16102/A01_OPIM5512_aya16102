6e5889bda3dc12bfb1c75db7678ce222d7490bb2
ahmedyosifahmed <ahmed.y.ahmed@uconn.edu>
2026-01-22 21:34:33 -0500

    Added line to ensure working directory can be located
    Changing the location of the script caused some issues when running the code. Adding this line ensures code can run smoothly!

--- src/boxplot.py
+++ src/boxplot.py
@@ -7,6 +7,9 @@ import os
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
 
+# Update this line to include your working directory !
+os.chdir(r"C:\Users\admin\Desktop\Grad_School\OPIM5512\Repository_Clones\A01\A01_OPIM5512_aya16102")
+
 # Load California Housing dataset
 housing = fetch_california_housing(as_frame=True)
 
